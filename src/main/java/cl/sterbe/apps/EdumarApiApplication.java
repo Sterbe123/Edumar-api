db1ab1442f0a3e0c2d0fef12a4581be2db560f33
Rodrigo Lazo <rodrigolazo.404@gmail.com>
2023-03-27 10:31:05 -0300

    Subiendo repositorio api edumar

---
+++ src/main/java/cl/sterbe/apps/EdumarApiApplication.java
@@ -0,0 +1,13 @@
+package cl.sterbe.apps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdumarApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdumarApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/sterbe/apps/EdumarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.sterbe.apps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdumarApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
